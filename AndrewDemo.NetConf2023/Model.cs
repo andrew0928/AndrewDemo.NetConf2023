ea06300090eca607fcc0ea22009d76a53158446b
Andrew Wu <chicken@chicken-house.net>
2023-12-04 01:07:43 +0800

    Add project files.

---
+++ AndrewDemo.NetConf2023/Model.cs
@@ -0,0 +1,147 @@
+﻿namespace AndrewDemo.NetConf2023
+{
+
+    public class Member
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+        public static Dictionary<int, Member> Database = new Dictionary<int, Member>()
+        {
+            { 1, new Member() { Id = 1, Name = "andrew" } },
+            { 2, new Member() { Id = 2, Name = "poy"} }
+        };
+    }
+
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public static Dictionary<int, Product> Database = new Dictionary<int, Product>()
+        {
+            { 1, new Product() { Id = 1, Name = "18天", Price = 65.00m } },
+            { 2, new Product() { Id = 2, Name = "可樂", Price = 18.00m} }
+        };
+    }
+
+
+    //public class SKU
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+
+
+    public class DiscountEngine
+    {
+        public static IEnumerable<DiscountRecord> Calculate(Cart cart, Member consumer)
+        {
+            // 18天 第二罐六折
+            var p = Product.Database.Where(p => p.Value.Name.Equals("18天")).FirstOrDefault().Value;
+            var pid = p.Id;
+
+            if (cart._ProdQtyMap.ContainsKey(pid) && cart._ProdQtyMap[pid] > 2)
+            {
+                for(int index = 1; index <= cart._ProdQtyMap[pid]; index++)
+                {
+                    if (index % 2 == 0) yield return new DiscountRecord()
+                    {
+                        Name = "18天 第二件六折優惠",
+                        DiscountAmount = p.Price * -0.4m,
+                    };
+                }
+            }
+        }
+
+        public class DiscountRecord
+        {
+            public string Name { get; set; }
+            public decimal DiscountAmount { get; set; }
+        }
+    }
+
+
+    public class Cart
+    {
+        public Dictionary<int, int> _ProdQtyMap = new Dictionary<int, int>();
+
+        // sku CRUD
+        public bool AddProducts(int productId, int qty = 1)
+        {
+            if (this._ProdQtyMap.ContainsKey(productId))
+            {
+                this._ProdQtyMap[productId] += qty;
+            }
+            else
+            {
+                this._ProdQtyMap[productId] = qty;
+            }
+            return true;
+        }
+    }
+
+
+    public class Checkout
+    {
+        private static int _serial_number = 1;
+        private static Dictionary<int, (int tid, Cart cart, Member consumer)> _temp = new Dictionary<int, (int tid, Cart cart, Member consumer)>();
+
+        public static int Create(Cart cart, Member consumer)
+        {
+            int tid = (_serial_number++);
+            _temp.Add(tid, (tid, cart, consumer));
+            return tid;
+        }
+
+        // confirm info / shipping / payment
+
+        public static Order CompleteWithPayment(int transactionId, int paymentId)
+        { 
+            var order = new Order();
+
+            var transaction = _temp[transactionId];
+            order.buyer = transaction.consumer;
+
+            decimal total = 0m;
+            foreach(var p in transaction.cart._ProdQtyMap)
+            {
+                Product product = Product.Database[p.Key];
+                int qty = p.Value;
+                total += product.Price * qty;
+
+                order.LineItems.Add((
+                    $"商品: {product.Name} (單價: {product.Price} x {qty} = {product.Price * qty}",
+                    product.Price * qty));
+            }
+
+            foreach(var dr in DiscountEngine.Calculate(transaction.cart, transaction.consumer))
+            {
+                order.LineItems.Add((
+                    $"優惠: {dr.Name} {dr.DiscountAmount}",
+                    dr.DiscountAmount));
+                total += dr.DiscountAmount;
+            }
+
+            order.TotalPrice = total;
+            order.Id = transactionId;
+
+            return order;
+        }
+    }
+
+
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public Member buyer;
+
+        public List<(string title, decimal price)> LineItems = new List<(string title, decimal price)>();
+
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ AndrewDemo.NetConf2023/Process.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndrewDemo.NetConf2023
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // login
+            Console.WriteLine("步驟 1, 登入");
+
+            var member = (from m in Member.Database where m.Value.Name == "andrew" select m.Value).FirstOrDefault();
+            Console.WriteLine($"user {member.Name}(id: {member.Id}) logged in.");
+
+            // browse product catalog
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("步驟 2, 瀏覽商品");
+            foreach (var p in Product.Database.Values)
+            {
+                Console.WriteLine($"- [{p.Id}] {p.Name}, ${p.Price}");
+            }
+
+            // add to shopping cart
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("步驟 3, 加入購物車, 帳款試算");
+
+            var cart = new Cart();
+            decimal total = 0m;
+            cart.AddProducts(1, 6);
+            cart.AddProducts(2, 1);
+            foreach(var ci in cart._ProdQtyMap)
+            {
+                Product p = Product.Database[ci.Key];
+                int pid = p.Id;
+                int qty = ci.Value;
+                Console.WriteLine($"- [{pid}] {p.Name}(單價: ${p.Price}) x {qty},     ${p.Price * qty}");
+                total += p.Price * qty;
+            }
+            foreach(var dr in DiscountEngine.Calculate(cart, member))
+            {
+                Console.WriteLine($"- [優惠] {dr.Name},   ${dr.DiscountAmount}");
+                total += dr.DiscountAmount;
+            }
+            Console.WriteLine($"結帳金額: ${total}");
+
+
+            // checkout
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("步驟 4, 結帳");
+
+            int tid = Checkout.Create(cart, member);
+            var order = Checkout.CompleteWithPayment(tid, 0);
+
+            // order created
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("步驟 5, 訂單成立");
+
+            Console.WriteLine($"oders info:");
+            Console.WriteLine($"訂單編號: {order.Id}");
+            Console.WriteLine($"買家: {order.buyer.Name} ({order.buyer.Id})");
+            Console.WriteLine($"明細:");
+            foreach(var lineitem in order.LineItems)
+            {
+                Console.WriteLine($"- [{lineitem.title}]    {lineitem.price}");
+            }
+            Console.WriteLine($"結帳金額: {order.TotalPrice}");
+
+
+
+            return;
+        }
+    }
+}
