c7e86900b7b5225d23ba94eadf66d313780c74fc
Andrew Wu <chicken@chicken-house.net>
2025-10-28 23:49:10 +0800

    clean up solutions

--- build.sh
+++ build.sh
@@ -39,10 +39,10 @@ done
 dotnet publish src/AndrewDemo.NetConf2023.sln -c Release /t:PublishContainer -m
 
 # tag images
-docker tag andrewdemo-netconf2023-api:develop         andrew0928.azurecr.io/andrewdemo-netconf2023-api:develop
-docker tag andrewdemo-netconf2023-api:$(date +%Y%m%d) andrew0928.azurecr.io/andrewdemo-netconf2023-api:$(date +%Y%m%d)
+docker tag andrewdemo-netconf2023-api:develop         andrew0928.azurecr.io/andrewdemo-shop-api:develop
+docker tag andrewdemo-netconf2023-api:$(date +%Y%m%d) andrew0928.azurecr.io/andrewdemo-shop-api:$(date +%Y%m%d)
 
 # push images
 if [ "$PUSH_IMAGES" = true ]; then
-    docker push andrew0928.azurecr.io/andrewdemo-netconf2023-api:$(date +%Y%m%d)
+    docker push andrew0928.azurecr.io/andrewdemo-shop-api:$(date +%Y%m%d)
 fi
--- src/AndrewDemo.NetCore2023.WebApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AndrewDemo.NetCore2023.WebApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
